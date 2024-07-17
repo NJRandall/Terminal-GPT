58ecf1551170f38308c2c771e544d35dcd56e047
NJRandall <nicholas.randall4@gmail.com>
2024-07-17 22:52:40 +1000

    added option to enable or disable audio generation and output formatting changes

--- chatgpt_character.py
+++ chatgpt_character.py
@@ -10,21 +10,36 @@ ELEVENLABS_VOICE = "Nickel" # Replace this with the name of whatever voice you h
 
 BACKUP_FILE = "ChatHistoryBackup.txt"
 
-print("[khaki1]initialising azureTTS...")
+print("[thistle3]preparing start up sequence...\n")
+
+print("Would you like audio output? Please enter 'y' for yes or 'n' for no:")
+
+while True:
+    if keyboard.is_pressed('y'):
+        print("You entered 'yes'.\n")
+        voice_generation = True
+        break
+    elif keyboard.is_pressed('n'):
+        print("You entered 'no'.\n")
+        voice_generation = False
+        break
+
+print("[thistle3]initialising azureTTS...")
 speechtotext_manager = SpeechToTextManager()
-print("[sea_green3]initialised azureTTS successfully")
+print("[medium_orchid3]initialised azureTTS successfully")
 
-print("[khaki1]initialising openAI...")
+print("[thistle3]initialising openAI...")
 openai_manager = OpenAiManager()
-print("[sea_green3]initialised openAI successfully")
+print("[medium_orchid3]initialised openAI successfully")
 
-print("[khaki1]initialising elevenlabs...")
-elevenlabs_manager = ElevenLabsManager()
-print("[sea_green3]initialised elevenlabs successfully")
-
-print("[khaki1]initialising audio manager...")
-audio_manager = AudioManager()
-print("[sea_green3]initialised audio manager\n")
+if voice_generation:
+    print("[thistle3]initialising elevenlabs...")
+    elevenlabs_manager = ElevenLabsManager()
+    print("[medium_orchid3]initialised elevenlabs successfully")
+    
+    print("[thistle3]initialising audio manager...")
+    audio_manager = AudioManager()
+    print("[medium_orchid3]initialised audio manager")
 
 
 FIRST_SYSTEM_MESSAGE = {"role": "system", "content": '''
@@ -38,18 +53,20 @@ While responding as Nickel, you must obey the following rules:
 3) You refer to the user as friend or a similar noun denoting a friendly relationship
 4) Frequently use phrases that consist of simple unsophisticated language. 
 5) Keep your answers limited to just a few sentences.
+6) When referred to as nicole treat as if referred to as nickel
                         
 Okay, let the conversation begin!'''}
 openai_manager.chat_history.append(FIRST_SYSTEM_MESSAGE)
 
-print("[sea_green3]Starting the loop, press F4 to begin")
+
+print("\nStarting the loop, press 'F4' to begin")
 while True:
     # Wait until user presses "f4" key
     if keyboard.read_key() != "f4":
         time.sleep(0.1)
         continue
 
-    print("[sea_green3]User pressed F4 key! Now listening to your microphone:")
+    print("User pressed 'F4' key! Now listening to your microphone:\n")
 
     # Get question from mic
     mic_result = speechtotext_manager.speechtotext_from_mic_continuous()
@@ -65,11 +82,12 @@ while True:
     with open(BACKUP_FILE, "w") as file:
         file.write(str(openai_manager.chat_history))
 
-    # Send it to 11Labs to turn into cool audio
-    elevenlabs_output = elevenlabs_manager.text_to_audio(openai_result, ELEVENLABS_VOICE, False)
+    if voice_generation:
+        # Send it to 11Labs to turn into cool audio
+        elevenlabs_output = elevenlabs_manager.text_to_audio(openai_result, ELEVENLABS_VOICE, False)
 
-    # Play the mp3 file
-    audio_manager.play_audio(elevenlabs_output, True, True, True)
+        # Play the mp3 file
+        audio_manager.play_audio(elevenlabs_output, True, True, True)
 
-    print("[sea_green3]\n!!!!!!!\nFINISHED PROCESSING DIALOGUE.\nREADY FOR NEXT INPUT\n!!!!!!!\n")
+    print("\n!!!!!!!!!!!!!!!\nFINISHED PROCESSING DIALOGUE.\nREADY FOR NEXT INPUT\nUse 'F4' to continue the conversation\n!!!!!!!!!!!!!!!\n")
     
